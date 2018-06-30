b339e3703a37bac4723be8230528f8d9a106031e
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-06-30 12:41:50 +0630

    set blank null to avoid circular dependency problem

--- mcq/models.py
+++ mcq/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.TextField()
-    correct_answer = models.ForeignKey('Answer', related_name='correct')
+    correct_answer = models.ForeignKey('Answer', related_name='correct', blank=True, null=True)
 
     def __str__(self):
         return self.question_text
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, blank=True, null=True)
     answer_text = models.TextField()
 
     def __str__(self):
