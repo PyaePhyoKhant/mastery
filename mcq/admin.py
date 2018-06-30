18b1a0988a9846907f230fbf72b284fcce20c97c
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-06-30 13:25:40 +0630

    inline in admin

--- mcq/admin.py
+++ mcq/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Question, Answer
 
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Answer)
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 4
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
+# Register your models here.
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
