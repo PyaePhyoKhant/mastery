0133e9d0bf5367578e4752eb5e1cacb05fd019d2
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-06-30 12:26:20 +0630

    initialization

---
+++ mcq/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.TextField()
+    correct_answer = models.ForeignKey('Answer', related_name='correct')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.TextField()
+
+    def __str__(self):
+        return self.answer_text
