215967d4dd532be8ef5e28530e2c2850375c8c17
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-06-30 13:08:07 +0630

    API for reverse FK

--- mcq/serializers.py
+++ mcq/serializers.py
@@ -2,7 +2,16 @@ from .models import Question, Answer
 from rest_framework import serializers
 
 
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+
+
 class QuestionSerializer(serializers.ModelSerializer):
+    answer_set = AnswerSerializer(many=True, read_only=True)
+
     class Meta:
         model = Question
-        fields = '__all__'
+        depth = 1
+        fields = ('id', 'question_text', 'answer_set', 'correct_answer')
