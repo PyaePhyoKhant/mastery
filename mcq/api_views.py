322758430518ab5230fc8b3e533e5c29aefe5510
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2018-07-01 00:55:44 +0630

    tournament answer

--- mcq/api_views.py
+++ mcq/api_views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
-from rest_framework.decorators import list_route
+from rest_framework.decorators import list_route, detail_route
 from rest_framework.response import Response
-from .models import Question, Answer, Tournament
+from .models import Question, Answer, Tournament, TournamentParticipation
 from .serializers import QuestionSerializer, QASerializer, TournamentSerializer
 
 
@@ -57,3 +57,33 @@ class QuestionViewSet(viewsets.ModelViewSet):
 class TournamentViewSet(viewsets.ModelViewSet):
     queryset = Tournament.objects.all()
     serializer_class = TournamentSerializer
+
+    @detail_route(methods=['POST', ], url_path='answer')
+    def answer(self, request, pk):
+        serializer = QASerializer(data=request.data)
+        if serializer.is_valid():
+            questions = serializer.data['questions']
+            answers = serializer.data['answers']
+            score = calculate_score(questions, answers)
+            if request.user.id is not None:
+                learner = request.user.learner_set.all()[0]
+                learner.points += score * 5
+                learner.save()
+                t = Tournament.objects.get(id=pk)
+                TournamentParticipation.objects.create(learner=learner, tournament=t, score=score)
+            # return_questions = []
+            # for i in range(len(questions)):
+            #     temp_dict = {}
+            #     q_id = int(questions[i])
+            #     a_id = int(answers[i])
+            #     q = Question.objects.get(id=q_id)
+            #     correct_id = None
+            #     for a in q.answer_set.all():
+            #         if a.correct:
+            #             correct_id = a.id
+            #             break
+            #     temp_dict['q_id'] = q_id
+            #     temp_dict['your_a_id'] = a_id
+            #     temp_dict['correct_a_id'] = correct_id
+            #     return_questions.append(temp_dict)
+            return Response(status=status.HTTP_200_OK)
